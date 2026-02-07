315c7c2223dcba64f8abb33de932a898efcf66e9
AuroraConst <auroraconstellations@gmail.com>
2026-02-07 13:51:13 -0500

    drawing on 2d canvas works!!

--- src/main/scala/org/aurora/Main.scala
+++ src/main/scala/org/aurora/Main.scala
@@ -12,6 +12,14 @@ import scala.scalajs.js.annotation.JSImport
 val _ = Stylesheet // Use import to prevent DCE
 
 @main def main(): Unit = {
+
+  val canvas = dom.document.getElementById("examplecanvas").asInstanceOf[dom.html.Canvas]
+  type Ctx2D = dom.CanvasRenderingContext2D
+  val ctx = canvas.getContext("2d").asInstanceOf[Ctx2D]
+  ctx.fillStyle = "blue"
+  ctx.fillRect(10, 10, 100, 100)
+  
+
   renderOnDomContentLoaded(
     container = dom.document.querySelector("#app"),
     rootNode = {
