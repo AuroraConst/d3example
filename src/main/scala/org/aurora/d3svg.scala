16d3a93ea2873058be845ace64868a73bebbcac2
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2026-02-08 11:38:40 -0500

    more clean up

--- src/main/scala/org/aurora/d3svg.scala
+++ src/main/scala/org/aurora/d3svg.scala
@@ -2,47 +2,37 @@ package org.aurora
 
 import typings.d3.mod as d3Mod
 
-import org.scalajs.dom.{document}
-import typings.std.global.{console, document, window}
-import typings.std.{ FrameRequestCallback, HTMLCanvasElement, stdStrings}
-
-
-import org.scalajs.dom.{CanvasRenderingContext2D,Console}
+import typings.std.global.{console, window}
 
 import scala.scalajs.js
-import scala.scalajs.js.|
-import org.scalajs.dom.{SVGCircleElement, SVGElement}
-import scala.scalajs.js.ThisFunction
-import scala.scalajs.js.ThisFunction3
-import typings.d3.d3Strings.th
+import org.scalajs.dom.{SVGCircleElement}
+import typings.d3Selection.mod.{ValueFn}
 
 
 object d3svg:
   case class CircleData(id: Double,radius:Double, color: String, x: Double, y: Double)
 
 
+  import js.JSConverters._
   val data = (1 to 5550).map{i =>
     CircleData(i, 10 + Math.random() * 20, s"hsl(${Math.random() * 360}, 100%, 50%)", Math.random() * window.innerWidth, Math.random() * window.innerHeight)
-  }.toSeq
+  }.toSeq.toJSArray
 
-  import js.JSConverters._
-  val dataAsJsArray = data.toJSArray
+
+  //this was hell figuring this out!!
+  def callback[SVGELEMENT,DATUM,R](f: (i:DATUM)=> R): ValueFn[SVGELEMENT, DATUM, R] =
+      (thisArg: SVGELEMENT, data: DATUM, index: Double, array: js.Array[SVGELEMENT] | typings.d3Selection.mod.ArrayLike[SVGELEMENT]) => f(data)
+
 
   def start(): Unit = 
+    console.info("Starting d3svg example")
     val svg = d3Mod.select(s"#$svgId")
       .attr("width", s"${window.innerWidth}px")
       .attr("height", s"${window.innerHeight}px")
       .style("border", "1px solid black")
 
-
-
-    import typings.d3Selection.mod.{ValueFn}
-
-    def callback[SVGELEMENT,DATUM,R](f: (i:DATUM)=> R): ValueFn[SVGELEMENT, DATUM, R] =
-        (thisArg: SVGELEMENT, data: DATUM, index: Double, array: js.Array[SVGELEMENT] | typings.d3Selection.mod.ArrayLike[SVGELEMENT]) => f(data)
-
     svg.selectAll[SVGCircleElement, CircleData]("circle")
-      .data(dataAsJsArray)
+      .data(data)
       .join("circle" )
       .attr("cx", callback((cd:CircleData) => cd.x) )
       .attr("cy", callback((cd:CircleData) => cd.y) )
